83b7a5c095cd55f0bd041ac09547ad80b3ef79d1
Massimiliano Arione <garakkio@gmail.com>
2014-03-25 20:06:51 +0100

    YESSSSSS

--- src/PUGRoma/Kata/Kata.php
+++ src/PUGRoma/Kata/Kata.php
@@ -8,7 +8,36 @@ class Kata
     {
         return true;
     }
+
+    /**
+     * @param integer
+     * @return string
+     */
+    public function fizzBuzz($numero)
+    {
+        $map = array(
+            15  => 'FizzBuzz',
+            3   => 'Fizz',
+            5   => 'Buzz',
+
+        );
+
+        foreach ($map as $chiave => $output) {
+            if ($numero % $chiave == 0) {
+                return $output;
+            }
+        }
+
+        return $numero;
+    }
+
+    /**
+     * @return string
+     */
+    public function stampa()
+    {
+        $array = array_map(array($this, 'fizzBuzz'), range(1, 100));
+
+        return implode(PHP_EOL, $array);
+    }
 }
-
-
-
